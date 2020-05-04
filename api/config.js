83db48b76d03d21132f7c5615bb8e1db70ef7bc9
John Fairclough <johnmfairclough@gmail.com>
2020-05-04 15:58:18 +0100

    api

---
+++ api/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  APP_ID: 'fe8c748e',
+  API_KEY: '20c3e73a59479724e90591efe9cdfa7d',
+  BASE_URL: 'https://api.adzuna.com/v1/api/jobs',
+  BASE_PARAMS: 'search/1?&results_per_page=20&content-type=application/json',
+};
