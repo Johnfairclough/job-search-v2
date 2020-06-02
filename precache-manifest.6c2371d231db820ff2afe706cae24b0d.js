ca28b0ca83d83b0d38f6e48a92bd072e38a4b8cc
John Fairclough <johnmfairclough@gmail.com>
2020-06-02 12:39:44 +0100

    Updates

---
+++ precache-manifest.6c2371d231db820ff2afe706cae24b0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b28f8bf7094e7a12d3c27b93e083d14",
+    "url": "/job-search-v2/index.html"
+  },
+  {
+    "revision": "30ebf62eb49cd6082bef",
+    "url": "/job-search-v2/static/css/main.2e42da8d.chunk.css"
+  },
+  {
+    "revision": "ae69b49d9b761cfb4a72",
+    "url": "/job-search-v2/static/js/2.ba250bb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/job-search-v2/static/js/2.ba250bb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30ebf62eb49cd6082bef",
+    "url": "/job-search-v2/static/js/main.0c405ea3.chunk.js"
+  },
+  {
+    "revision": "770197306071f1a303c6",
+    "url": "/job-search-v2/static/js/runtime-main.3fa685f3.js"
+  }
+]);
